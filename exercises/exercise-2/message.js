128afe44dc0ef42b5ffe65e6c313c781e85fc8c2
Chris Price <christopher.allen.price@gmail.com>
2019-01-08 14:24:46 -0500

    Exercise 2-1 and 2-2

--- exercises/exercise-2/message.js
+++ exercises/exercise-2/message.js
@@ -3,8 +3,12 @@ import { View, Text, StyleSheet } from 'react-native';
 
 export default class Message extends React.Component {
   render() {
+    const message = 'hello!';
+    const isAwesome = true;
+
     return (
       <View>
+        <Text>Here's a message for you: {message} {isAwesome ? 'AWESOME' : 'NOT AWESOME' }</Text>
       </View>
     );
   }  
