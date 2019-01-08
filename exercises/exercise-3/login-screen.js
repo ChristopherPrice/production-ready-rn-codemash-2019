47b16549bfa3b9dfb3e35cb64c47deb72b81836a
Chris Price <christopher.allen.price@gmail.com>
2019-01-08 15:44:02 -0500

    Completing exercise 3-3

--- exercises/exercise-3/login-screen.js
+++ exercises/exercise-3/login-screen.js
@@ -2,25 +2,42 @@ import React from 'react';
 import { View, Text, Button, TextInput, Alert, StyleSheet } from 'react-native';
 
 export default class LoginScreen extends React.Component {
+  state = {
+    isLoggedIn: false,
+    username: ''
+  };
+  
   render() {
     return (
       <View style={styles.container}>
         <TextInput
           onSubmitEditing={() => {}}
+          onChangeText={(text) => {this.handleUsernameChange(text)}}
           placeholder="Username"
+          value={this.state.username}
         />
         <Button 
           onPress={() => {this.handlePress()}}
           title="Login"
         />
-        <Text style={styles.loginStatus}>You are not currently logged in.</Text>
+        <Text style={styles.loginStatus}>{this.state.isLoggedIn ? `You are logged in as ${this.state.username}` : 'You are not currently logged in.'}</Text>
       </View>
     );
   }
 
   handlePress() {
-    Alert.alert('Your message here!');
-  }  
+    if (this.state.username.length > 3) {
+      this.setState({
+        isLoggedIn: true
+      });
+    }
+  }
+
+  handleUsernameChange(text) {
+    this.setState({
+      username: text
+    });
+  }
 }
 
 const styles = StyleSheet.create({
